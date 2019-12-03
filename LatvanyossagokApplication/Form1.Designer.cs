2a92978bf39c57c304f5c7c6b0a720e75812a040
szamona <szantomomka@gmail.com>
2019-12-03 18:38:04 +0100

    tablak letrehozasa,varosok listazasa

--- LatvanyossagokApplication/Form1.Designer.cs
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxVarosok = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonHozzaad = new System.Windows.Forms.Button();
+            this.numericUpDownLakossag = new System.Windows.Forms.NumericUpDown();
+            this.listBoxVarosok = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLakossag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Város nev:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lakosság száma:";
+            // 
+            // textBoxVarosok
+            // 
+            this.textBoxVarosok.Location = new System.Drawing.Point(97, 14);
+            this.textBoxVarosok.Name = "textBoxVarosok";
+            this.textBoxVarosok.Size = new System.Drawing.Size(91, 20);
+            this.textBoxVarosok.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBoxVarosok);
+            this.panel1.Controls.Add(this.buttonHozzaad);
+            this.panel1.Controls.Add(this.numericUpDownLakossag);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxVarosok);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 221);
+            this.panel1.TabIndex = 4;
+            // 
+            // buttonHozzaad
+            // 
+            this.buttonHozzaad.Location = new System.Drawing.Point(98, 69);
+            this.buttonHozzaad.Name = "buttonHozzaad";
+            this.buttonHozzaad.Size = new System.Drawing.Size(75, 23);
+            this.buttonHozzaad.TabIndex = 5;
+            this.buttonHozzaad.Text = "Hozzáadás";
+            this.buttonHozzaad.UseVisualStyleBackColor = true;
+            this.buttonHozzaad.Click += new System.EventHandler(this.ButtonHozzaad_Click);
+            // 
+            // numericUpDownLakossag
+            // 
+            this.numericUpDownLakossag.Location = new System.Drawing.Point(98, 43);
+            this.numericUpDownLakossag.Name = "numericUpDownLakossag";
+            this.numericUpDownLakossag.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDownLakossag.TabIndex = 3;
+            // 
+            // listBoxVarosok
+            // 
+            this.listBoxVarosok.FormattingEnabled = true;
+            this.listBoxVarosok.Location = new System.Drawing.Point(36, 108);
+            this.listBoxVarosok.Name = "listBoxVarosok";
+            this.listBoxVarosok.Size = new System.Drawing.Size(137, 108);
+            this.listBoxVarosok.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLakossag)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxVarosok;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonHozzaad;
+        private System.Windows.Forms.NumericUpDown numericUpDownLakossag;
+        private System.Windows.Forms.ListBox listBoxVarosok;
     }
 }
 
--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace LatvanyossagokApplication
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conn;
         public Form1()
         {
             InitializeComponent();
+            conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
+            conn.Open();
+            varosokTablaLetrehozas();
+            latvanyosagokTablaLetrehozas();
         }
+
+
+        void latvanyosagokTablaLetrehozas()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS `latvanyossagok` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `leiras` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `ar` int(11) NOT NULL DEFAULT '0',
+                                  `varos_id` int(11) NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  KEY `varos_id` (`varos_id`),
+                                FOREIGN KEY(varos_id) REFERENCES varosok (id)
+                                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
+            cmd.ExecuteNonQuery();
+        }
+        void varosokTablaLetrehozas()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS `varosok` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `nev` varchar(128) COLLATE utf8_hungarian_ci NOT NULL,
+                                  `lakossag` int(11) NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE KEY `nev_indx` (`nev`)
+                                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;";
+            cmd.ExecuteNonQuery();
+        }
+
+        void VarosListazas()
+        {
+            listBoxVarosok.Items.Clear();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT `id`, `nev`, `lakossag` FROM `varosok`";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32("id");
+                    var nev = reader.GetString("nev");
+                    var lakossag = reader.GetInt32("lakossag");
+                    listBoxVarosok.Items.Add(nev + "– Lakosság: " + lakossag + " fő");
+                }
+            }
+        }
+
+        private void ButtonHozzaad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
